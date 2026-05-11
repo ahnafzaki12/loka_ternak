07aa28574a4a3a01d059e3d17eeadbcf40f1bf8b
ahnafzaki12 <ahnafzaki.b@gmail.com>
2026-05-11 09:19:22 +0700

    feat: initialize Express backend with Prisma ORM and essential project dependencies

--- backend-express/index.js
+++ backend-express/index.js
@@ -3,17 +3,21 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 3001;
 
-// Middleware
 app.use(cors());
-app.use(express.json()); // Agar bisa menerima body format JSON
+app.use(express.json());
 
-// Route Contoh
+app.get('/', (req, res) => {
+    res.send('Backend berjalan!');
+});
+
 app.get('/api/halo', (req, res) => {
-    res.json({ pesan: "Halo dari Express JS" });
+    res.json({
+        pesan: 'Halo dari Express JS'
+    });
 });
 
 app.listen(PORT, () => {
-    console.log(`Server berjalan di http://localhost:${PORT}`);
+    console.log(`Server berjalan di http://127.0.0.1:${PORT}`);
 });
