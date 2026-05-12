dc22d6a6d92ef178b24ca8ec65e50a2e9950be1e
ahnafzaki12 <ahnafzaki.b@gmail.com>
2026-05-12 09:12:32 +0700

    masih error

--- backend-express/index.js
+++ backend-express/index.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-
-// Import Routes
-const authRoutes = require('./src/routes/auth.routes');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
-
-app.use(cors());
-app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send('Backend berjalan!');
+    res.send('OK');
 });
 
-// Gunakan Auth Routes 
-app.use('/api/auth', authRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Server berjalan di http://127.0.0.1:${PORT}`);
+app.listen(5000, () => {
+    console.log('RUNNING ON 5000');
 });
--- backend-express/src/lib/prisma.js
+++ backend-express/src/lib/prisma.js
@@ -1,5 +1,23 @@
-const { PrismaClient } = require('@prisma/client');
+require('dotenv').config();
 
-const prisma = new PrismaClient();
+const { PrismaClient } = require('@prisma/client');
+const { PrismaNeon } = require('@prisma/adapter-neon');
+const { Pool, neonConfig } = require('@neondatabase/serverless');
+
+const ws = require('ws');
+
+neonConfig.webSocketConstructor = ws;
+
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({
+    connectionString,
+});
+
+const adapter = new PrismaNeon(pool);
+
+const prisma = new PrismaClient({
+    adapter,
+});
 
 module.exports = prisma;
